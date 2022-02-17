f95e0b52ac89335766bbb5a5ab12af3abae285f3
Himanshu Rana <himanshurjj@gmail.com>
2022-02-18 01:13:39 +0530

    .

--- DynamicProgramming/LongestCommonSubstring.java
+++ DynamicProgramming/LongestCommonSubstring.java
@@ -19,13 +19,40 @@ public class LongestCommonSubstring {
 
     }
 
+    public int longestCommonSubstringDP(String s, String t) {
+
+        int dp[][] = new int[s.length() + 1][t.length() + 1];
+
+        for(int i = 0; i <= s.length(); i++) {
+            dp[i][0] = 0;
+        }
+        for(int i = 0; i <= t.length(); i++) {
+            dp[0][i] = 0;
+        }
+
+        int result = 0;
+
+        for(int i = 1; i <= s.length(); i++) {
+            for(int j = 1; j <= t.length(); j++) {
+                if(s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    result = Math.max(result, dp[i][j]);
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         
-        String s = "abcde";
-        String t = "abcde";
+        String s = "cabd";
+        String t = "zabde";
         System.out.println("The Ans --------->");
         LongestCommonSubstring lcsObj = new LongestCommonSubstring();
-        lcsObj.longestCommonSubstringRecursion(s, t, 0);
-        System.out.println(lcsObj.result);
+        int result = lcsObj.longestCommonSubstringDP(s, t);
+        System.out.println(result);
     }
 }
