5808ec78cdca484c6dd8c16d51036890e29d31bd
Himanshu Rana <himanshurjj@gmail.com>
2022-04-04 08:06:16 +0530

    .;

--- Trees/Traversal.java
+++ Trees/Traversal.java
@@ -1,5 +1,9 @@
 package Trees;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 class Node {
     
     int data;
@@ -21,10 +25,47 @@ public class Traversal {
             return;
         }
 
-        System.out.println(root.data + " ");
+        System.out.print(root.data + " ");
 
         preOrderTraversal(root.left);
         preOrderTraversal(root.right);
+    }
+
+    public void levelOrderTraversal(Node root) {
+        Queue<Node> pendingNodes = new LinkedList<>();
+        if(root == null) {
+            return;
+        }
+        pendingNodes.add(root);
+        while(!pendingNodes.isEmpty()) {
+            Node currentNode = (Node)pendingNodes.poll();
+            System.out.println(currentNode.data);
+
+            if(currentNode.left != null) {
+                pendingNodes.add(currentNode.left);
+            }
+            if(currentNode.right != null) {
+                pendingNodes.add(currentNode.right);
+            }            
+        }
+    }
+
+    public void preOrderTraversalIterative(Node root) {
+        Stack<Node> pendingNodes = new Stack<>();
+        if(root == null) {
+            return;
+        }
+        pendingNodes.add(root);
+        while(!pendingNodes.isEmpty()) {
+            Node currentNode = pendingNodes.pop();
+            System.out.print(currentNode.data + " ");
+            if(currentNode.right != null) {
+                pendingNodes.add(currentNode.right);
+            }
+            if(currentNode.left != null) {
+                pendingNodes.add(currentNode.left);
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -36,5 +77,7 @@ public class Traversal {
         root.left.right = new Node(5);
         Traversal traversalObj = new Traversal();
         traversalObj.preOrderTraversal(root);
+        System.out.println();
+        traversalObj.preOrderTraversalIterative(root);
     }
 }
