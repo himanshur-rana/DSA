b8fc52ad41d0e1c9239ac4ae4513ab8c95ca76bc
Himanshu Rana <himanshurjj@gmail.com>
2022-04-06 07:40:06 +0530

    .

--- Trees/Traversal.java
+++ Trees/Traversal.java
@@ -93,7 +93,55 @@ public class Traversal {
         }
     }
 
+    public void postOrderTraversalIterative1(Node root) {
+        // This is using 2 stacks
+        if(root == null) {
+            return;
+        }
+        Stack<Node> st1 = new Stack<>();
+        Stack<Node> st2 = new Stack<>();
+        st1.push(root);
+        while(!st1.isEmpty()) {
+            Node poppedNode = st1.pop();
+            st2.push(poppedNode);
+            if(poppedNode.left != null) {
+                st1.push(poppedNode.left);
+            }
+            if(poppedNode.right != null) {
+                st1.push(poppedNode.right);
+            }
+        }
+        while(!st2.empty()) {
+            System.out.print(st2.pop().data + " ");
+        }
+    }
 
+    public void postOrderTraversalIterative2(Node root) {
+        // Using 1 stack
+        if(root == null) {
+            return;
+        }
+        Stack<Node> st = new Stack<>();
+        Node currentNode = root;
+        while(currentNode != null || !st.isEmpty()) {
+            if(currentNode != null) {
+                st.push(currentNode);
+                currentNode = currentNode.left;                
+            } else {
+                Node temp = st.peek().right;
+                if(temp == null) {
+                    temp = st.pop();
+                    System.out.print(temp.data + " ");
+                    while(!st.isEmpty() && st.peek().right == temp) {
+                        temp = st.pop();
+                        System.out.print(temp.data + " ");
+                    }
+                } else {
+                    currentNode = temp;
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) {
         // Make a tree
@@ -103,6 +151,8 @@ public class Traversal {
         root.left.left = new Node(4);
         root.left.right = new Node(5);
         Traversal traversalObj = new Traversal();
-        traversalObj.inorderTraversalIterative(root);
+        traversalObj.postOrderTraversalIterative1(root);
+        System.out.println();
+        traversalObj.postOrderTraversalIterative2(root);
     }
 }
